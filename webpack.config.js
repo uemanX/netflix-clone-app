5d0abfae77cd12b8e9cf38a37bc2d2fd09087297
ueman <p20ym25q@gmail.com>
2021-08-08 18:04:25 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// prod dev dev-serverで分けるか？
+
+module.exports = {
+  mode: "development",
+
+  entry: "./src/index.tsx",
+  output: {
+    path: `${__dirname}/public/dist`,
+    filename: "index.js",
+
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader"
+      }
+    ]
+  },
+  // import 文で .ts や .tsx ファイルを解決するため
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  // ES5(IE11等)向けの指定（webpack 5以上で必要）
+  // target: ["web", "es5"],
+  target: "web",
+
+  devServer: {
+    contentBase: "./public",
+    host: 'localhost',
+    port: 3000, 
+    watchContentBase: true,
+    historyApiFallback: true,
+    hot: true,
+    writeToDisk: true,
+    open: true
+  }
+};
