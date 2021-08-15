8d2cd70e605204f7038cf39d436a10df9deadfd4
ueman <p20ym25q@gmail.com>
2021-08-15 23:14:53 +0900

    feat: Implementing  firebase authentication.

--- src/components/SignupScreen/index.tsx
+++ src/components/SignupScreen/index.tsx
@@ -1,23 +1,59 @@
 import './SignupScreen.css';
-import React from 'react';
+import React, { useRef } from 'react';
+import { auth } from '../../firebase';
 
 type Props = {};
 
 const SignupScreen = (props: Props) => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
   const register = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
     e.preventDefault();
+
+    if (!emailRef.current?.value || !passwordRef.current?.value) {
+      return;
+    }
+
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current?.value,
+        passwordRef.current?.value,
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   const signIn = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
+
+    if (!emailRef.current?.value || !passwordRef.current?.value) {
+      return;
+    }
+
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current?.value,
+        passwordRef.current?.value,
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   return (
     <div className='signupScreen'>
       <form>
         <h1>Sign In</h1>
-        <input type='email' placeholder='Email' />
-        <input type='password' placeholder='Password' />
+        <input ref={emailRef} type='email' placeholder='Email' />
+        <input ref={passwordRef} type='password' placeholder='Password' />
         <button type='submit' onClick={signIn}>
           Sign In
         </button>
