ee86b60364e09cb9f0ad779e4d1e8f0919e7b766
ueman <p20ym25q@gmail.com>
2021-08-17 23:01:59 +0900

    feat: Implementing Redux.

---
+++ src/features/user/userSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../../store';
+
+export type UserState = {
+  user: {
+    uid: string;
+    email: string;
+  } | null;
+};
+
+const initialState: UserState = {
+  user: null,
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+    },
+    // increment: (state) => {
+    //   return {
+    //     ...state,
+    //     // value: state.value + 1,
+    //   };
+    // },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+
+export const selectUser = (state: RootState) => state.user.user;
+
+export default userSlice.reducer;
