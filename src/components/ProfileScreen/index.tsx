ede44402caaac56efd5d18dd71af3c7cf012502f
ueman <p20ym25q@gmail.com>
2021-08-18 21:59:14 +0900

    feat: Implementing profile component.

---
+++ src/components/ProfileScreen/index.tsx
@@ -0,0 +1,41 @@
+import './ProfileScreen.css';
+import React from 'react';
+import Nav from '../Nav';
+import { useAppSelector } from '../../hooks';
+import { selectUser } from '../../features/user/userSlice';
+import { auth } from '../../firebase';
+
+type Props = {};
+
+const ProfileScreen = (props: Props) => {
+  const user = useAppSelector(selectUser);
+
+  return (
+    <div className='profileScreen'>
+      <Nav />
+      <div className='profileScreen__body'>
+        <h1>Edit Profile</h1>
+        <div className='profileScreen__info'>
+          <img
+            src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+            alt=''
+          />
+          <div className='profileScreen__details'>
+            <h2>{user?.email}</h2>
+            <div className='profileScreen__plans'>
+              <h3>Plans</h3>
+              <button
+                onClick={() => auth.signOut()}
+                className='profileScreen__signOut'
+              >
+                Sign Out
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileScreen;
